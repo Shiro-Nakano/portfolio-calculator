e325118ee81fe8889cc5fef3cebf6891455de4fc
Shiro-Nakano <nakano.shiro2001@gmail.com>
2025-11-29 12:38:48 +0900

    別ファイルからの移動

---
+++ calculator/src/Display/IDisplay.ts
@@ -0,0 +1,11 @@
+/** 
+ * インターフェース　IDisplay:ディスプレイ表示
+ * 【Private】
+ * +render(text: string)　　　　　　：途中式や入力時の数字の表示メソッド
+ * +renderError(message: string)　：エラー表示時のメソッド
+ */
+
+export interface IDisplay{
+    render(text: string):void;
+    renderError(message: string):void;
+}
