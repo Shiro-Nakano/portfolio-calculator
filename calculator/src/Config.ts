aaadfd486eb6cd2d138103e963a8acace544335e
Shiro-Nakano <nakano.shiro2001@gmail.com>
2025-11-29 12:48:03 +0900

    別ファイルからの移動

---
+++ calculator/src/Config.ts
@@ -0,0 +1,10 @@
+/** 
+ * クラス：Config            : 電卓全体で扱う定数
+ * 【定数】
+ * MAX_DIGITS      number   :
+ * ERROR_MESSAGE   string   :
+ */
+export class Config{
+    static readonly MAX_DIGITS: number = 8;
+    static readonly ERROR_MESSAGE: string = "エラー";
+}
